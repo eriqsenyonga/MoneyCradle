f4a59025987979abbe2b9c54b756838e5d3e203e
eriqsenyonga <senyonga@gmail.com>
2016-11-19 21:22:33 +0300

    new pc sync

--- sentayzo/build/generated/source/aidl/debug/com/android/vending/billing/IInAppBillingService.java
+++ sentayzo/build/generated/source/aidl/debug/com/android/vending/billing/IInAppBillingService.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\Android Studio Projects\\MoneyCradle\\Sentayzo\\sentayzo\\src\\main\\aidl\\com\\android\\vending\\billing\\IInAppBillingService.aidl
+ * Original file: E:\\Android Studio Projects\\MoneyCradle\\Sentayzo\\sentayzo\\src\\main\\aidl\\com\\android\\vending\\billing\\IInAppBillingService.aidl
  */
 package com.android.vending.billing;
 /**
